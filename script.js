faa77a8bbcc7cc701a1216f2cd6aeabae728e90a
Sajal <sajalzaki@gmail.com>
2016-06-03 12:58:39 -0400

    new change

---
+++ script.js
@@ -0,0 +1,13 @@
+function check(){
+	if (document.getElementById('username')<=0)
+	{
+		alert("you didnt enter username");
+	}
+	else if (document.getElementById('pass')!=12345678)
+	{
+		alert("you entered wrong password");
+	}
+
+
+
+}
